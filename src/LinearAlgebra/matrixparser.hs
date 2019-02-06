34786f0b7b6df54c8afe8b9f824281798e70feff
jmacmanus <josephmacmanus11@gmail.com>
2019-02-06 13:33:50 +0000

    intMatrix parser working, tho possibly a bit wonky with what it accepts.

--- src/LinearAlgebra/matrixparser.hs
+++ src/LinearAlgebra/matrixparser.hs
@@ -1,4 +1,7 @@
-module LinearAlgebra.MatrixParser where
+module LinearAlgebra.MatrixParser
+( matrix
+, intMatrix
+) where
 
 {-
 GOAL: parse matrices on input, in the form of [[a]].
@@ -7,15 +10,20 @@ i.e. [[1,2],[3,4]]
 we can allow for {} instead of [] too, possibly.
 -}
 
-import Data.Matrix
+import Data.Matrix hiding (matrix)
 
 -- | Small parser library
 import Text.Yoda
 
-class Parsable a where
-  mparser :: Parser a
+list :: Parser a -> Parser [a]
+list px = between (token "[") (token "]") (
+        many (px <* optional (token ",")))
 
--- list :: Parser [a]
+matrix :: Parser a -> Parser (Matrix a)
+matrix px = fromLists <$> list (list px)
+
+intMatrix :: Parser (Matrix Integer)
+intMatrix = matrix integer
 
 -- useful parsers
 
@@ -26,12 +34,12 @@ token :: String -> Parser String
 token t = string t <* whitespace
 
 -- changed from Int to Integer, might break something
-number :: Parser Integer
-number = read <$> some (oneOf ['0'..'9']) <* whitespace
+integer :: Parser Integer
+integer = read <$> some (oneOf ['0'..'9']) <* whitespace
 
 -- TODO parse doubles in a similar way to the above.
 -- double :: Parser Double
--- double = read <$> some (oneOf ['0'..'9']))
+-- double = read <$> (some (oneOf ['0'..'9']))
 
 word :: Parser String
 word = some (oneOf ['a'..'z']) <* whitespace
